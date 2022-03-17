3f008fd20469e6c38d76e4d1206bec54d6e0cf04
younghyun Ju <kunkun7762@gmail.com>
2022-03-17 13:48:44 +0900

    #1.3 ObjectTyp

---
+++ src/restaurants/restaurants.resolver.ts
@@ -0,0 +1,10 @@
+import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Restaurant } from './entities/restaurant.entity';
+
+@Resolver(() => Restaurant)
+export class RestaurantsResolver {
+  @Query(() => [Restaurant])
+  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    return [];
+  }
+}
